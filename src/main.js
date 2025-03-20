27815b4069d8f2686c0a04cae87b8dab631b967a
Austin Huang <im@austinhuang.me>
2025-03-19 23:45:14 -0400

    feat: re-enable alpinejs

--- src/main.js
+++ src/main.js
@@ -1,35 +1,13 @@
 import './styles/main.css';
 
-// import Alpine from 'alpinejs';
-// import persist from '@alpinejs/persist';
+import Alpine from 'alpinejs';
+import persist from '@alpinejs/persist';
 
-// window.Alpine = Alpine;
+window.Alpine = Alpine;
 
-// Alpine.plugin(persist);
+Alpine.plugin(persist);
 
-// document.addEventListener('alpine:init', () => {
-//   Alpine.data('banner', function () {
-//     return {
-//       show: this.$persist(false),
-//       dismissed: this.$persist(false),
-
-//       dismiss() {
-//         this.show = false;
-//         this.dismissed = true;
-//       },
-
-//       init() {
-//         if (!this.dismissed) {
-//           setTimeout(() => {
-//             this.show = true;
-//           }, 1500);
-//         }
-//       },
-//     };
-//   });
-// });
-
-// Alpine.start();
+Alpine.start();
 
 const env = document.querySelector('body').dataset.env;
 
