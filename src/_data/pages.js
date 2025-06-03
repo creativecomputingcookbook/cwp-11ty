9c9fdae8548ee56aab202eb11667ea8d40b574d0
Austin Huang <im@austinhuang.me>
2025-06-03 13:00:03 -0400

    feat: database-driven pagination

---
+++ src/_data/pages.js
@@ -0,0 +1,22 @@
+const Firestore = require('@google-cloud/firestore'), slugify = require('slugify');
+
+const db = new Firestore({
+  projectId: 'cwp-11ty',
+});
+const collection = db.collection('cwp-pages');
+
+module.exports = async () => {
+    let pages = [];
+    let s = await collection.get();
+    s.forEach(doc => {
+        let d = doc.data();
+        let prefix = ""
+        if (d.tags.indexOf("Foundations") > -1) prefix = "foundations";
+        if (d.tags.indexOf("Builds") > -1) prefix = "builds";
+        pages.push({
+            ...d,
+            permalink: `/${prefix}/${slugify(d.title)}/`,
+        });
+    });
+    return pages;
+}
