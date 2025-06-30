94f36ed58ba33c6ab011bcf13538ab6cc9a2cbe9
Mathew Ponon <mop9047@nyu.edu>
2025-06-30 15:35:15 -0400

    revert changes to pages.js

--- src/_data/pages.js
+++ src/_data/pages.js
@@ -1,5 +1,4 @@
-const Firestore = require('@google-cloud/firestore'),
-  slugify = require('slugify');
+const Firestore = require('@google-cloud/firestore'), slugify = require('slugify');
 
 const db = new Firestore({
   projectId: 'cwp-11ty',
@@ -7,19 +6,18 @@ const db = new Firestore({
 const collection = db.collection('cwp-pages');
 
 module.exports = async () => {
-  let pages = [];
-  let s = await collection.get();
-  s.forEach((doc) => {
-    let d = doc.data();
-    let prefix = '';
-    if (d.tags.indexOf('Foundations') > -1) prefix = 'foundations';
-    if (d.tags.indexOf('Builds') > -1) prefix = 'builds';
-    pages.push({
-      ...d,
-      prefix,
-      permalink: `/${prefix}/${slugify(d.title)}/`,
+    let pages = [];
+    let s = await collection.get();
+    s.forEach(doc => {
+        let d = doc.data();
+        let prefix = ""
+        if (d.tags.indexOf("Foundations") > -1) prefix = "foundations";
+        if (d.tags.indexOf("Builds") > -1) prefix = "builds";
+        pages.push({
+            ...d,
+            prefix,
+            permalink: `/${prefix}/${slugify(d.title)}/`,
+        });
     });
-  });
-  console.log(pages);
-  return pages;
-};
+    return pages;
+}
