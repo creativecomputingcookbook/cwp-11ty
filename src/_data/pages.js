27922059441f868f49a7a9c775f9c57cf5fdaa96
Mathew Ponon <mop9047@nyu.edu>
2025-06-30 13:48:21 -0400

    Video tab and temporary fix for related pages

--- src/_data/pages.js
+++ src/_data/pages.js
@@ -1,4 +1,5 @@
-const Firestore = require('@google-cloud/firestore'), slugify = require('slugify');
+const Firestore = require('@google-cloud/firestore'),
+  slugify = require('slugify');
 
 const db = new Firestore({
   projectId: 'cwp-11ty',
@@ -6,18 +7,19 @@ const db = new Firestore({
 const collection = db.collection('cwp-pages');
 
 module.exports = async () => {
-    let pages = [];
-    let s = await collection.get();
-    s.forEach(doc => {
-        let d = doc.data();
-        let prefix = ""
-        if (d.tags.indexOf("Foundations") > -1) prefix = "foundations";
-        if (d.tags.indexOf("Builds") > -1) prefix = "builds";
-        pages.push({
-            ...d,
-            prefix,
-            permalink: `/${prefix}/${slugify(d.title)}/`,
-        });
+  let pages = [];
+  let s = await collection.get();
+  s.forEach((doc) => {
+    let d = doc.data();
+    let prefix = '';
+    if (d.tags.indexOf('Foundations') > -1) prefix = 'foundations';
+    if (d.tags.indexOf('Builds') > -1) prefix = 'builds';
+    pages.push({
+      ...d,
+      prefix,
+      permalink: `/${prefix}/${slugify(d.title)}/`,
     });
-    return pages;
-}
+  });
+  console.log(pages);
+  return pages;
+};
