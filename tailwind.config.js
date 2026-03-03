42baea61039951419e3d65c7171c48350babc166
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-03-03 16:44:09 -0500

    feat: add design system tokens, fonts, and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['General Sans', ...defaultTheme.fontFamily.sans],
+        mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        cream: '#fffaef',
+        'cream-dark': '#fff3dc',
+        peach: '#f7cb9c',
+        'sky-light': '#9cd3f7',
+        lime: '#d8ff76',
+        sand: '#c0b6a2',
+      },
+      boxShadow: {
+        button: '-6px 6px 0px rgba(0, 0, 0, 0.48)',
+        frame: '0px -8px 0px rgba(129, 121, 104, 0.5)',
       },
     },
   },
